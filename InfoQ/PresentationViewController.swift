8115fe8f2228260699b651dba11bff18c9eba36e
Uwe Dauernheim <uwe@dauernheim.net>
2014-08-06 15:32:12 +0200

    Replace MediaPlayer with AVPlayer
    This also fixes the movie load bug
    
    Change-Id: I6a5213c794e9dceaae94525f023d0ac000252fe5

--- InfoQ/PresentationViewController.swift
+++ InfoQ/PresentationViewController.swift
@@ -1,10 +1,11 @@
+import AVFoundation
+import AVKit
 import UIKit
-import MediaPlayer
 
 class PresentationViewController: UIViewController {
 
     var presentation: Presentation?
-    var moviePlayer: MPMoviePlayerController?
+    var playerViewController: AVPlayerViewController?
     lazy var api: APIController = APIController()
 
     
@@ -16,59 +17,58 @@ class PresentationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let DIM_WIDTH = 320
-        let MARGIN = 10
-        let WIDTH = DIM_WIDTH - 2*MARGIN
+        // TODO: Use AutoLayout
+        let DIM_WIDTH: CGFloat = 320
+        let MARGIN: CGFloat = 10
+        let WIDTH: CGFloat = DIM_WIDTH - 2 * MARGIN
+        let DEFAULT_ASPECT_RATIO: CGFloat = 1.0 //0.5625
+        var OFFSET = MARGIN
 
         let title = UILabel()
-        title.frame = CGRect(x: MARGIN, y: 1 * MARGIN + 0, width: WIDTH, height: 100)
+        title.frame = CGRect(x: MARGIN, y: OFFSET, width: WIDTH, height: 100.0)
         title.numberOfLines = 0
         title.textAlignment = .Center
         title.font = .boldSystemFontOfSize(22)
         title.backgroundColor = .whiteColor()
         title.text = presentation!.title
+        OFFSET += MARGIN + 100.0
 
         let author = UILabel()
-        author.frame = CGRect(x: MARGIN, y: 2 * MARGIN + 100, width: WIDTH, height: 20)
+        author.frame = CGRect(x: MARGIN, y: OFFSET, width: WIDTH, height: 20.0)
         author.numberOfLines = 1
         author.textAlignment = .Center
         author.backgroundColor = .whiteColor()
         author.text = "by \(presentation!.authorsFormatted!)"
+        OFFSET += MARGIN + 20.0
 
         let summary = UILabel()
-        summary.frame = CGRect(x: MARGIN, y: 3 * MARGIN + 100+20, width: WIDTH, height: 150)
+        summary.frame = CGRect(x: MARGIN, y: OFFSET, width: WIDTH, height: 150.0)
         summary.numberOfLines = 0
         summary.textAlignment = .Center
         summary.backgroundColor = .whiteColor()
         summary.text = presentation!.summary
+        OFFSET += MARGIN + 150.0
 
-        moviePlayer = MPMoviePlayerController()
-        moviePlayer!.view.frame = CGRect(x: MARGIN, y: 4 * MARGIN + 100+20+150, width: WIDTH, height: 150)
-        moviePlayer!.movieSourceType = MPMovieSourceType.Unknown
-        moviePlayer!.controlStyle = MPMovieControlStyle.Embedded
+        playerViewController = AVPlayerViewController()
+        playerViewController!.view.frame = CGRect(
+            x: MARGIN,
+            y: OFFSET,
+            width: WIDTH,
+            height: WIDTH * DEFAULT_ASPECT_RATIO)
+        OFFSET += WIDTH * DEFAULT_ASPECT_RATIO
 
         api.fetchPresentation(presentation!.id!) { presentation in
-            logInfo("Preparing \(presentation.videoUrl!.description)")
-            self.moviePlayer!.movieSourceType = MPMovieSourceType.Unknown
-            self.moviePlayer!.contentURL = presentation.videoUrl
-            self.moviePlayer!.prepareToPlay()
-            self.moviePlayer!.play()
-            println(self.moviePlayer!.contentURL)
-            println(self.moviePlayer!.errorLog)
-            println(self.moviePlayer!.accessLog)
-            logInfo("Playing \(presentation.videoUrl!.description)")
+            self.playerViewController!.player = AVPlayer.playerWithURL(presentation.videoUrl) as AVPlayer
+            // TODO: Make asynchronous
+            self.adjustPlayerViewFrameSize()
         }
 
         let scrollView = UIScrollView(frame: view.frame)
         scrollView.addSubview(title)
         scrollView.addSubview(author)
         scrollView.addSubview(summary)
-        scrollView.addSubview(moviePlayer!.view)
-
-        // Calculate content size based on last/latest subview offset
-        let lastSubView = scrollView.subviews.last as UIView
-        let bottomOffset = lastSubView.frame.origin.y + lastSubView.frame.size.height
-        scrollView.contentSize = CGSize(width: CGFloat(DIM_WIDTH), height: bottomOffset)
+        scrollView.addSubview(playerViewController!.view)
+        scrollView.contentSize = CGSize(width: CGFloat(DIM_WIDTH), height: OFFSET)
 
         view.backgroundColor = UIColor.whiteColor()
         view.addSubview(scrollView)
@@ -79,6 +79,18 @@ class PresentationViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func adjustPlayerViewFrameSize() {
+        let movieSize = playerViewController!.player.currentItem.presentationSize
+        let playerFrame = playerViewController!.view.frame
+        let aspectRatio = movieSize.height / movieSize.width
+        let adjustedPlayerFrame = CGRect(
+            x: playerFrame.origin.x,
+            y: playerFrame.origin.y,
+            width: playerFrame.width,
+            height: playerFrame.width * aspectRatio)
+        playerViewController!.view.frame = adjustedPlayerFrame
     }
 
 }
